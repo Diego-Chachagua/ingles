54cf825b6fde9b8bd5ef67da72c46c0c6e344815
FernandoRodriguez12 <fernandojoseguinea.rodriguez@gmail.com>
2023-07-09 21:45:36 -0600

    more changes

--- lib/screens/ver_n_a.dart
+++ lib/screens/ver_n_a.dart
@@ -74,65 +74,145 @@ class _VerNotasAEState extends State<VerNotasA> {
             backgroundColor: Colors.transparent,
             body: SingleChildScrollView(
                 child: Column(
-                  children: [
-                    const SizedBox(height: 20,),
-                    Row(
-              children: const [
-                SizedBox(width: 20,),
-                    Text("Filtros:",style: TextStyle(
-                      color: Color.fromARGB(255, 87, 51, 235),
-                      fontSize: 25,
-                    ),)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-               Container(
-                width: 70,
-                height: 31,
-                decoration:  BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(width: 2),
-                  borderRadius: BorderRadius.all(Radius.circular(10))
+                const SizedBox(
+                  height: 20,
                 ),
-                child: Center(child: Text("Año",style: TextStyle(color: Colors.white,fontSize: 20),)),
-               ),
-               Container(
-                width: 70,
-                height: 31,
-                decoration:  BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(width: 2),
-                  borderRadius: BorderRadius.all(Radius.circular(10))
-                ),
-                child: Center(child: Text("Seccion",style: TextStyle(color: Colors.white,fontSize: 20),)),
-               ),
-               Container(
-                width: 70,
-                height: 31,
-                decoration:  BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(width: 2),
-                  borderRadius: BorderRadius.all(Radius.circular(10))
-                ),
-                child: Center(child: Text("Unidad",style: TextStyle(color: Colors.white,fontSize: 20),)),
-               )
-               ,
-               MaterialButton(onPressed: (){
-
-               },
-                child: const SizedBox(
-                              height: 45,
-                              width: 45,
-                              child: Image(image: AssetImage('assets/lupa.png'))),
-               ),
-               
-              ],
-            ),
-            Container(width: 1000,color: Colors.black,height: 2,),
-            
+                Row(
+                  children: const [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "Filtros:",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 87, 51, 235),
+                        fontSize: 25,
+                      ),
+                    )
                   ],
-                ))));
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      width: 70,
+                      height: 31,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          border: Border.all(width: 2),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      child: Center(
+                          child: Text(
+                        "Año",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )),
+                    ),
+                    Container(
+                      width: 70,
+                      height: 31,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          border: Border.all(width: 2),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      child: Center(
+                          child: Text(
+                        "Seccion",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )),
+                    ),
+                    Container(
+                      width: 70,
+                      height: 31,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          border: Border.all(width: 2),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      child: const Center(
+                          child: Text(
+                        "Unidad",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )),
+                    ),
+                    //espacio para edificion de boton de busqueda
+                    MaterialButton(
+                      onPressed: () {},
+                      child: const SizedBox(
+                          height: 45,
+                          width: 45,
+                          child: Image(image: AssetImage('assets/lupa.png'))),
+                    ),
+                    //fin de espacio para edición de boton de busqueda
+                  ],
+                ),
+                //espacio para definicion de linea
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: 1000,
+                  color: Colors.black,
+                  height: 2,
+                ),
+                //fin de espacio para definicion de linea
+                const SizedBox(
+                  height: 20,
+                ),
+                //espacio para definición de contenedor para mostrar historial
+                Container(
+                  width: 320,
+                  height: 80,
+                  decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(15)),
+                      color: Colors.grey,
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.black,
+                      )),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row( 
+                        children: [
+                          const SizedBox(
+                            width: 30,
+                          ),
+                          const Text("Name of Activity",style: TextStyle(fontSize: 15),),
+                          const SizedBox(
+                            width: 110,
+                          ),
+                          Column(
+                            children: const[
+                               SizedBox(
+                            height: 40,
+                          ),
+                              Text("U1",style: TextStyle(fontSize: 15),),
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+
+                          //espacio para definición de linea horizontal
+                          Container(
+                            width: 2,
+                            height: 76,
+                            decoration: BoxDecoration(
+                                border:
+                                    Border.all(width: 2, color: Colors.black)
+                                //fin de espacio de linea horizontal
+                                ),
+                          ),
+                          const SizedBox(width:10),
+                          const Text("0/10",style: TextStyle(fontSize: 15),),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                //fin de definición de contenedor 
+
+              ],
+            ))));
   }
 }
