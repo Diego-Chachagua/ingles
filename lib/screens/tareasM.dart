369100d86aa1c84c86c91b0e561227260cca9d49
FernandoRodriguez12 <fernandojoseguinea.rodriguez@gmail.com>
2023-07-17 07:08:48 -0600

    new files

---
+++ lib/screens/tareasM.dart
@@ -0,0 +1,65 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+import '../main.dart';
+import 'elec_e_o_t.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: OpAlum(),
+  ));
+}
+
+class OpAlum extends StatefulWidget {
+  const OpAlum({super.key});
+
+  @override
+  State<OpAlum> createState() => _OpAlumEState();
+}
+
+class _OpAlumEState extends State<OpAlum> {
+  final usuariob = TextEditingController();
+  final contrab = TextEditingController();
+
+  String usuariobd = "";
+  String contrabd = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/fondop.jpg'), fit: BoxFit.cover),
+        ),
+        child: Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              elevation: 0,
+              backgroundColor: const Color.fromARGB(0, 255, 255, 255),
+              title: Center(
+                child: GradientText(
+                  'ELIJA UNA OPCION',
+                  style: const TextStyle(
+                    fontSize: 30.0,
+                  ),
+                  gradientType: GradientType.linear,
+                  gradientDirection: GradientDirection.ttb,
+                  radius: 3.5,
+                  colors: const [
+                    Color.fromARGB(255, 170, 63, 233),
+                    Color.fromARGB(255, 66, 91, 233),
+                    Color.fromARGB(255, 60, 135, 221),
+                  ],
+                ),
+              ),
+            ),
+            backgroundColor: Colors.transparent,
+            body: SingleChildScrollView(
+                child: Center(
+                )
+            )
+        )
+    );
+  }
+}
